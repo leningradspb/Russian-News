347143e62ca7cff24dfd71bc913937b1a974787c
Ed <sinyakov.gsg@gmail.com>
2020-01-20 02:29:55 +0300

    works

--- Russian News/Controllers/MainVC.swift
+++ Russian News/Controllers/MainVC.swift
@@ -209,12 +209,12 @@ extension MainVC: UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
 
-        return UIEdgeInsets(top: 5, left: 20, bottom: 5, right: 20)
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
 
-        return 5
+        return 1
     }
 	
 }
